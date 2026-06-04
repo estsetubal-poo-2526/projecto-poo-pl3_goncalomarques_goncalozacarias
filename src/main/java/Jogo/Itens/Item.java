ae078f0f35aee6e61ad3bee60f59e19d57a67256
Gonçalo Zacarias <2025176280@estudantes.ips.pt>
2026-06-04 17:40:02 +0100

    Ajusta Item ao novo construtor de ObjetoMovel

--- src/main/java/Jogo/Itens/Item.java
+++ src/main/java/Jogo/Itens/Item.java
@@ -1,11 +1,14 @@
 package Jogo.Itens;
 
-import Jogo.Objetos.ObjetoMovel;
 import Jogo.Nave.Jogador.NaveJogador;
+import Jogo.Objetos.ObjetoMovel;
 
 public abstract class Item extends ObjetoMovel {
-    public Item(double velocidadeX, double velocidadeY) {
-        super(velocidadeX, velocidadeY);
+
+    public Item(double posX, double posY, double largura, double altura, double velocidade,
+                double velocidadeX, double velocidadeY) {
+        super(posX, posY, largura, altura, velocidade, velocidadeX, velocidadeY);
     }
+
     public abstract NaveJogador aplicarEfeito();
 }
