0c5f4fa8f8c6b47a6db17475f6d2a073c1ba7103
Gonçalo Zacarias <2025176280@estudantes.ips.pt>
2026-06-04 17:28:07 +0100

    Construtores + gettes e setters na classe

--- src/main/java/Jogo/Objetos/ObjetoJogo.java
+++ src/main/java/Jogo/Objetos/ObjetoJogo.java
@@ -2,9 +2,60 @@ package Jogo.Objetos;
 
 public abstract class ObjetoJogo {
     private double posX;
-    private double poxY;
+    private double posY;
     private double largura;
     private double altura;
     private double velocidade;
-    public void atualizar(){}
+
+    public ObjetoJogo(double posX, double posY, double largura, double altura, double velocidade) {
+        this.posX = posX;
+        this.posY = posY;
+        this.largura = largura;
+        this.altura = altura;
+        this.velocidade = velocidade;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void atualizar() {
+
+    }
 }
