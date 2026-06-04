362ce2bf7fb05852444d8a93133e7575584e7a8f
Goncalo-213 <2025175329@estudantes.ips.pt>
2026-06-04 18:14:35 +0100

    Coloquei a label no topo e evento no botao sair

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -10,15 +10,24 @@ import javafx.scene.*;
 
 public class App extends Application {
     public void start(Stage menu) {
-        Button botaoIniciar = new Button("Iniciar");
+        Button botaoIniciar = new Button("Começar Jogo");
         Button botaoMelhoria = new Button("Melhorias");
         Button botaoSair = new Button("Sair");
+        botaoSair.setOnAction(event -> {
+            menu.close();
+        });
         Label labelInicial = new Label("Bem vindo");
-        VBox vbox = new VBox(15);
-        vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(botaoIniciar, botaoMelhoria, botaoSair, labelInicial);
+        HBox topo = new HBox();
+        VBox direita = new VBox(50);
+        direita.setAlignment(Pos.CENTER_RIGHT);
+        direita.autosize();
+        direita.getChildren().addAll(botaoIniciar, botaoMelhoria, botaoSair);
+        labelInicial.autosize();
+        topo.getChildren().add(labelInicial);
+        topo.setAlignment(Pos.TOP_CENTER);
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(vbox);
+        borderPane.setCenter(direita);
+        borderPane.setTop(topo);
         Scene scene = new Scene(borderPane, 400, 300);
         menu.setTitle("SpaceWars");
         menu.setScene(scene);
