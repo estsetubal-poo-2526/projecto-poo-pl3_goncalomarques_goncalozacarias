8de3286484a0c4cc01338093874489c8dcd8d389
Goncalo-213 <2025175329@estudantes.ips.pt>
2026-06-04 18:35:29 +0100

    testes no view

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -9,6 +9,16 @@ import javafx.scene.*;
 
 
 public class App extends Application {
+
+    //Stage e scenes que serão reutilizadas
+    private Stage primaryStage;
+    private Scene menuScene, dificuldadeScene, melhoriasScene;
+
+    private final String fundo_preto = "-fx-background-color: black";
+    private final String fundo_azul = "-fx-background-color: black";
+    private final String fundo_roxo = "-fx-background-color: black";
+    private final String fundo_tete = "-fx-background-color: black";
+
     public void start(Stage menu) {
         Button botaoIniciar = new Button("Começar Jogo");
         Button botaoMelhoria = new Button("Melhorias");
@@ -16,17 +26,18 @@ public class App extends Application {
         botaoSair.setOnAction(event -> {
             menu.close();
         });
-        Label labelInicial = new Label("Bem vindo");
+        Label labelInicial = new Label("SPACEWARS");
         HBox topo = new HBox();
-        VBox direita = new VBox(50);
-        direita.setAlignment(Pos.CENTER_RIGHT);
-        direita.autosize();
-        direita.getChildren().addAll(botaoIniciar, botaoMelhoria, botaoSair);
+        VBox vbox = new VBox(50);
+        vbox.setAlignment(Pos.CENTER);
+        labelInicial.setStyle("-fx-font-size: 20px");
+        vbox.setStyle("-fx-font-size: 20px");
+        vbox.getChildren().addAll(botaoIniciar, botaoMelhoria, botaoSair);
         labelInicial.autosize();
         topo.getChildren().add(labelInicial);
         topo.setAlignment(Pos.TOP_CENTER);
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(direita);
+        borderPane.setCenter(vbox);
         borderPane.setTop(topo);
         Scene scene = new Scene(borderPane, 400, 300);
         menu.setTitle("SpaceWars");
