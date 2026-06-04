d783713d29111caf89eef95adc63a1bbcf14982a
Gonçalo Zacarias <2025176280@estudantes.ips.pt>
2026-06-04 17:35:05 +0100

    Adaptar a classe

--- src/main/java/Jogo/Objetos/ObjetoMovel.java
+++ src/main/java/Jogo/Objetos/ObjetoMovel.java
@@ -4,15 +4,31 @@ public abstract class ObjetoMovel extends ObjetoJogo {
     private double velocidadeX;
     private double velocidadeY;
 
-    public ObjetoMovel(double velocidadeX, double velocidadeY) {
+    public ObjetoMovel(double posX, double posY, double largura, double altura, double velocidade,
+                       double velocidadeX, double velocidadeY) {
+        super(posX, posY, largura, altura, velocidade);
         this.velocidadeX = velocidadeX;
         this.velocidadeY = velocidadeY;
     }
-    public double getVelocidadeX(){
+
+    public double getVelocidadeX() {
         return velocidadeX;
     }
-    public double getVelocidadeY(){
+
+    public void setVelocidadeX(double velocidadeX) {
+        this.velocidadeX = velocidadeX;
+    }
+
+    public double getVelocidadeY() {
         return velocidadeY;
     }
-    //test
+
+    public void setVelocidadeY(double velocidadeY) {
+        this.velocidadeY = velocidadeY;
+    }
+
+    public void mover() {
+        setPosX(getPosX() + velocidadeX);
+        setPosY(getPosY() + velocidadeY);
+    }
 }
