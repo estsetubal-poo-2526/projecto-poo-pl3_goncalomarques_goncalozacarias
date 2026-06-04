42f2b16034c6c6a19235c993b381d4c9c5e36797
Gonçalo Zacarias <2025176280@estudantes.ips.pt>
2026-06-04 17:14:50 +0100

    Adicionei metodos na classe nave

--- src/main/java/Jogo/Nave/Nave.java
+++ src/main/java/Jogo/Nave/Nave.java
@@ -5,8 +5,33 @@ import Jogo.Objetos.ObjetoJogo;
 public abstract class Nave extends ObjetoJogo {
     private int vidas;
     private int dano;
-    public Nave(int vidas, int dano){
+
+    public Nave(int vidas, int dano) {
         this.vidas = vidas;
         this.dano = dano;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    public int getDano() {
+        return dano;
+    }
+
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+
+    public void perderVida(int quantidade) {
+        vidas -= quantidade;
+    }
+
+    public void aumentarVida(int quantidade) {
+        vidas += quantidade;
     }
 }
