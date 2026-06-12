8c6b76a3e2d36fa135eab9d5c1983765119f65b1
Gonçalo Zacarias <2025176280@estudantes.ips.pt>
2026-06-12 14:42:08 +0100

    Criação das Exceções

---
+++ src/main/java/Jogo/Excecoes/MoedasInsuficientesException.java
@@ -0,0 +1,7 @@
+package Jogo.Excecoes;
+
+public class MoedasInsuficientesException extends Exception {
+    public MoedasInsuficientesException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/Jogo/Excecoes/NivelMaximoMelhoriaException.java
@@ -0,0 +1,7 @@
+package Jogo.Excecoes;
+
+public class NivelMaximoMelhoriaException extends Exception {
+    public NivelMaximoMelhoriaException(String mensagem) {
+        super(mensagem);
+    }
+}
